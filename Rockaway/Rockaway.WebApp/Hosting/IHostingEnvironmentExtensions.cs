7ad5f15e264cf67fcc7ffd463948cf49d9a03fef
Dylan Beattie <dylan@dylanbeattie.net>
2024-04-16 16:02:57 +0200

    All the things...

---
+++ Rockaway/Rockaway.WebApp/Hosting/IHostingEnvironmentExtensions.cs
@@ -0,0 +1,11 @@
+// Rockaway.WebApp/Hosting/HostEnvironmentExtensions.cs
+
+namespace Rockaway.WebApp.Hosting;
+
+public static class HostEnvironmentExtensions {
+	private static readonly string[] sqliteEnvironments
+		= { "UnitTest", Environments.Development };
+
+	public static bool UseSqlite(this IHostEnvironment env)
+	=> sqliteEnvironments.Contains(env.EnvironmentName);
+}
