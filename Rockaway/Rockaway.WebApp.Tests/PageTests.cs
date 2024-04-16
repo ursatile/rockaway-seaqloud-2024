e5f6bc953acdf676a45264944be8629ab1d777e5
Dylan Beattie <dylan@dylanbeattie.net>
2024-04-16 15:20:38 +0200

    Remove duplicated code from PageTests

--- Rockaway/Rockaway.WebApp.Tests/PageTests.cs
+++ Rockaway/Rockaway.WebApp.Tests/PageTests.cs
@@ -3,8 +3,6 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Rockaway.WebApp.Data;
 using Shouldly;
-using System.Net;
-using Rockaway.WebApp.Data.Sample;
 
 namespace Rockaway.WebApp.Tests;
 
@@ -31,26 +29,5 @@ public class PageTests {
 		var h1 = dom.QuerySelector("h1");
 		h1.ShouldNotBeNull();
 		h1.InnerHtml.ShouldBe("Welcome, Seaqloud!");
-	}
-}
-
-
-
-public class ArtistTests {
-
-	public static IEnumerable<string> SampleArtists() {
-		return SampleData.Artists.AllArtists.Select(a => a.Name);
-	}
-	[Theory]
-	[MemberData(nameof(SampleArtists))]
-	public async Task Artist_Page_Contains_All_Artists(string artistName) {
-		await using var factory = new WebApplicationFactory<Program>();
-		var client = factory.CreateClient();
-		var html = await client.GetStringAsync("/artists");
-		var decodedHtml = WebUtility.HtmlDecode(html);
-		//using var scope = factory.Services.CreateScope();
-		//var db = scope.ServiceProvider.GetService<RockawayDbContext>()!;
-		//var expected = db.Artists.ToList();
-		decodedHtml.ShouldContain(artistName);
 	}
 }
